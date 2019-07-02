d45a36ce12df795e047e14087352b40f02550c1c
treyg21 <41927921+treyg21@users.noreply.github.com>
2019-07-02 18:20:22 -0400

    initial

---
+++ main.js
@@ -0,0 +1,11 @@
+function classToggle() {
+  const navs = document.querySelectorAll('.Navbar__Items')
+  
+  navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
+}
+document.querySelector('.Navbar__Link-toggle')
+  .addEventListener('click', classToggle);
+
+
+
+  
