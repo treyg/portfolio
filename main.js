ba12ac23d6106da939e6dd8cb23d9e6cc6c96035
treyg <tag@treyg.dev>
2021-12-29 20:05:21 -0500

    Refactor CSS to be responsive

--- main.js
+++ main.js
@@ -2,25 +2,23 @@
   let mainNav = document.getElementById('js-menu');
   let navBarToggle = document.getElementById('js-navbar-toggle');
   navBarToggle.addEventListener('click', function () {
-      mainNav.classList.toggle('active');
+    mainNav.classList.toggle('active');
   });
   // Modal popup for footer
-    // Get the modal
+  // Get the modal
   var modal = document.getElementById("myModal");
   // Get the button that opens the modal
   var btn = document.getElementById("myBtn");
   // Get the <span> element that closes the modal
   var span = document.getElementsByClassName("close")[0];
   // When the user clicks the button, open the modal 
-  btn.onclick = function() {
-   modal.style.display = "block";
+  btn.onclick = function () {
+    modal.style.display = "block";
   }
   // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-    modal.style.display = "none";
-  }
+  document.querySelector('.close').addEventListener('click', () => modal.style.display = "none")
   // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
+  window.onclick = function (event) {
     if (event.target == modal) {
       modal.style.display = "none";
     }
